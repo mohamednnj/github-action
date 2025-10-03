d46d2cb56a785fb14f38402abb8415f2e141c909
sirri <mohamedelsary960@gmail.com>
2025-10-03 19:55:14 +0300

    use checkout action

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send.json({test: 'test result a json format'});
+})
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
