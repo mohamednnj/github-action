bf31fbb0c75b389133fbc3fa7dd706bcf2a27744
Mohamed Al-Sirri <110845833+mohamednnj@users.noreply.github.com>
2025-10-11 02:49:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files (CSS, JS, Images)
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+// Return index.html
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.json({test: 'test result a json format'});
-})
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
